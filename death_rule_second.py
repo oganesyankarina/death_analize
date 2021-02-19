c8489694c8a1e2f91ac0a95ace2a44a1d21e82a4
oganesyankarina <oganesyankarina@mail.ru>
2021-02-19 16:35:57 +0300

    В output добавлено поле uuid, для output исп-ся ф-ции из make_task_table

--- death_rule_second.py
+++ death_rule_second.py
@@ -6,8 +6,9 @@ import logging
 
 from connect_PostGres import cnx
 from ISU_death_lists_dict import df_Population, REGION, FIO_dict, MKB_GROUP_LIST_MAIN
-# from get_from_death_finished import get_df_death_finished
 from ISU_death_functions import time_factor_calculation, get_df_death_finished, get_db_last_index
+import uuid
+from make_task_table import make_recipient, make_corr_for_recipient, make_release_date, make_recipient_fio
 
 
 def death_rule_second_new(save_to_sql=True, save_to_excel=False):
@@ -17,7 +18,6 @@ def death_rule_second_new(save_to_sql=True, save_to_excel=False):
     print(f'{program} started')
 
     df_death, YEARS, MONTHS, DATES, GENDERS, AGE_GROUPS = get_df_death_finished()
-    # df_death_finished = df_death_finished
 
     # Расчет показателей
     # Таблица с численностью населения
@@ -127,43 +127,28 @@ def death_rule_second_new(save_to_sql=True, save_to_excel=False):
     # Формируем результат работы и записываем в БД
     print('Формируем перечень задач, назначаем ответственных и сроки...')
     output = pd.DataFrame(columns=['recipient', 'message', 'deadline', 'release',
-                                   'task_type', 'title', 'fio_recipient'])
-
+                                   'task_type', 'title', 'fio_recipient', 'uuid'])
     k = get_db_last_index('test_output')
-    # if len(pd.read_sql_query('''SELECT * FROM public."test_output"''', cnx)) == 0:
-    #     k = 0
-    # else:
-    #     k = pd.read_sql_query('''SELECT * FROM public."test_output"''', cnx).id.max() + 1
-
     for i in results_blowout.index:
-        mo = results_blowout.loc[i, 'Region']
-        MKB = results_blowout.loc[i, 'MKB']
-        if (mo == 'Липецк') | (mo == 'Елец'):
-            corr = ''
-        else:
-            corr = 'район'
-        recipient = f'Главный врач ЦРБ {mo} {corr}'
-        message = f'Проанализировать и принять меры по снижению смертности. На протяжении последних трех месяцев в районе наблюдается рост смертности от заболеваний из Группы {MKB}'
+        recipient = make_recipient(results_blowout.loc[i, 'Region'])
+        fio = make_recipient_fio(recipient)
+        release = make_release_date(results_blowout.loc[i, 'DATE'])
 
         MKB_id = MKB_GROUP_LIST_MAIN.index(results_blowout.loc[i, 'MKB'])
         task_type = f'Смертность_П2_new1_{MKB_id}'
 
-        if results_blowout.loc[i, 'DATE'].month == 12:
-            release = date(results_blowout.loc[i, 'DATE'].year + 1, 1, 1)
-        else:
-            release = date(results_blowout.loc[i, 'DATE'].year, results_blowout.loc[i, 'DATE'].month + 1, 1)
+        MKB = results_blowout.loc[i, 'MKB']
+        message = f'Проанализировать и принять меры по снижению смертности. На протяжении последних трех месяцев в районе наблюдается рост смертности от заболеваний из Группы {MKB}'
 
         title = f'Рост смертности от заболеваний из группы {MKB}'
-        if recipient in FIO_dict.keys():
-            FIO = FIO_dict[recipient]
-        else:
-            FIO = ''
+
         output.loc[k] = {'task_type': task_type,
                          'recipient': recipient,
                          'message': f'ИСУ обычная {message}',
                          'release': release,
                          'deadline': str(date.today() + pd.Timedelta(days=14)),
-                         'title': title, 'fio_recipient': FIO}
+                         'title': title, 'fio_recipient': fio,
+                         'uuid': uuid.uuid3(uuid.NAMESPACE_DNS, recipient + message + str(release))}
         k += 1
 
     print('Сохраняем результаты...')
@@ -230,45 +215,28 @@ def death_rule_second_new(save_to_sql=True, save_to_excel=False):
 
     # Формируем результат работы и записываем в БД
     output = pd.DataFrame(columns=['recipient', 'message', 'deadline', 'release', 'task_type',
-                                   'title', 'fio_recipient'])
-
+                                   'title', 'fio_recipient', 'uuid'])
     k = get_db_last_index('test_output')
-    # if len(pd.read_sql_query('''SELECT * FROM public."test_output"''', cnx)) == 0:
-    #     k = 0
-    # else:
-    #     k = pd.read_sql_query('''SELECT * FROM public."test_output"''', cnx).id.max() + 1
-
     for i in results_blowout.index:
-        mo = results_blowout.loc[i, 'Region']
-        MKB = results_blowout.loc[i, 'MKB']
-
-        if (mo == 'Липецк') | (mo == 'Елец'):
-            corr = ''
-        else:
-            corr = 'район'
-
-        recipient = f'Главный врач ЦРБ {mo} {corr}'
-        message = f'Проанализировать и принять меры по снижению смертности. В районе по сравнению с аналогичным периодом прошлого года наблюдается значительный рост смертности от заболеваний из Группы {MKB}'
+        recipient = make_recipient(results_blowout.loc[i, 'Region'])
+        fio = make_recipient_fio(recipient)
+        release = make_release_date(results_blowout.loc[i, 'DATE'])
 
         MKB_id = MKB_GROUP_LIST_MAIN.index(MKB)
         task_type = f'Смертность_П2_new2_{MKB_id}'
 
-        if results_blowout.loc[i, 'DATE'].month == 12:
-            release = date(results_blowout.loc[i, 'DATE'].year + 1, 1, 1)
-        else:
-            release = date(results_blowout.loc[i, 'DATE'].year, results_blowout.loc[i, 'DATE'].month + 1, 1)
+        MKB = results_blowout.loc[i, 'MKB']
+        message = f'Проанализировать и принять меры по снижению смертности. В районе по сравнению с аналогичным периодом прошлого года наблюдается значительный рост смертности от заболеваний из Группы {MKB}'
 
         title = f'Рост смертности от заболеваний из группы {MKB} по сравнению с АППГ'
-        if recipient in FIO_dict.keys():
-            FIO = FIO_dict[recipient]
-        else:
-            FIO = ''
+
         output.loc[k] = {'task_type': task_type,
                          'recipient': recipient,
                          'message': f'ИСУ обычная {message}',
                          'release': release,
                          'deadline': str(date.today() + pd.Timedelta(days=14)),
-                         'title': title, 'fio_recipient': FIO}
+                         'title': title, 'fio_recipient': fio,
+                         'uuid': uuid.uuid3(uuid.NAMESPACE_DNS, recipient + message + str(release))}
         k += 1
 
     print('Сохраняем результаты...')
