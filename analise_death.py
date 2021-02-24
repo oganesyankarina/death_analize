8e9b73b5db948c6c222971bcaca8f56bcd9b5ed7
oganesyankarina <oganesyankarina@mail.ru>
2021-02-24 13:34:56 +0300

    мелкие корректировки, загрузка данных из БД

--- analise_death.py
+++ analise_death.py
@@ -3,6 +3,9 @@
 import pandas as pd
 import csv
 
+from connect_PostGres import cnx
+from ISU_death_functions import make_date
+
 path = r'C:\Users\oganesyanKZ\PycharmProjects\ISU_death\Рассчеты/'
 file = f'{path}death_202102011133.csv'
 
@@ -26,12 +29,6 @@ def read_csv(url):
     return data
 
 
-def MakeDate(df, date_col):
-    for col in date_col:
-        df[col] = df[col].apply(pd.to_datetime)
-    return df
-
-
 def MakeDateBornDeath(df, date_col):
     for i in df.index:
         df.loc[i, 'ДАТА_РОЖДЕНИЯ'] = df.loc[i, date_col[0]].date()
@@ -39,8 +36,9 @@ def MakeDateBornDeath(df, date_col):
         df.loc[i, 'ДАТА_СЕРТИФИКАТА'] = df.loc[i, date_col[2]].date()
     return df
 
-
-def MakeMonthYearDeath(df):
+def make_month_year_death(df):
+    df['МЕСЯЦ_СМЕРТИ'] = 0
+    df['ГОД_СМЕРТИ'] = 0
     for i in df.index:
         df.loc[i, 'МЕСЯЦ_СМЕРТИ'] = int(df.loc[i, 'ДАТА_СМЕРТИ'].month)
         df.loc[i, 'ГОД_СМЕРТИ'] = int(df.loc[i, 'ДАТА_СМЕРТИ'].year)
@@ -56,7 +54,8 @@ def CalcDateDelta(df, date_col):
     return df
 
 if __name__ == '__main__':
-    df = read_csv(file)
+    # df = read_csv(file)
+    df = pd.read_sql_query('''SELECT * FROM death''', cnx)
     df.columns = ['id', 'sex', 'birth', 'death', 'address_full', 'locality', 'at_death', 'locality2',
                   'place_death', 'family', 'education', 'occupation', 'reason_death', 'reason_established',
                   'reason_a', 'period_reason_a', 'original_reasons_a', 'reason_b', 'period_reason_b',
@@ -65,11 +64,11 @@ if __name__ == '__main__':
                   'date_issue_certificate']
 
     print('Обрабатываем данные...')
-    df = MakeDate(df, ['birth', 'death', 'date_issue_certificate'])
+    df = make_date(df, ['birth', 'death', 'date_issue_certificate'])
     print('MakeDate done')
     df = MakeDateBornDeath(df, ['birth', 'death', 'date_issue_certificate'])
     print('MakeDateBornDeath done')
-    df = MakeMonthYearDeath(df)
+    df = make_month_year_death(df)
     print('MakeDayWeekMonthYearDeath done')
     df = CalcDateDelta(df, ['ДАТА_СМЕРТИ', 'ДАТА_СЕРТИФИКАТА'])
     print('CalcDateDelta done')
