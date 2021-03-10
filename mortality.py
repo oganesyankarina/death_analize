d73b40c8c4e3d54888419b14bb4056ba9e8cad08
oganesyankarina <oganesyankarina@mail.ru>
2021-03-10 10:20:36 +0300

    Из импорта убраны 'неиспользуемые' библиотеки, функции и переменные

--- mortality.py
+++ mortality.py
@@ -1,27 +1,5 @@
-import pandas as pd
-import numpy as np
-import uuid
 import logging
-import statsmodels.api as sm
-from datetime import date, datetime
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-
-from connect_PostGres import cnx
-
-from ISU_death_lists_dict import df_Population, REGION, MONTHS_dict
-from ISU_death_lists_dict import not_nan_filter, MKB_CODE_LIST, MKB_GROUP_LIST, MKB_GROUP_LIST_MAIN, df_MKB
-from ISU_death_lists_dict import FIO_dict, escalation_recipient_list, escalation_recipient_text, df_FIO
-from ISU_death_lists_dict import results_files_path, results_files_suff
-
-from ISU_death_functions import make_date, make_date_born_death, make_day_week_month_year_death, calculate_death_age
-from ISU_death_functions import calculate_age_group, calculate_employee_group, make_mkb, make_address
-from ISU_death_functions import find_original_reason_mkb_group_name
-from ISU_death_functions import time_factor_calculation, get_df_death_finished, get_db_last_index
-from ISU_death_functions import make_recipient, make_corr_for_recipient, make_release_date, make_recipient_fio
-from ISU_death_functions import get_db_last_index, make_escalation_recipient_fio, make_recipient_fio
-from ISU_death_functions import time_factor_calculation, get_df_death_finished, get_db_last_index
-from ISU_death_functions import make_recipient, make_corr_for_recipient, make_release_date, make_recipient_fio
+from datetime import datetime
 
 from preprocessing import death_preprocessing
 from death_rule_first_55 import death_rule_first_55
@@ -41,7 +19,7 @@ if __name__ == '__main__':
         death_preprocessing(save_to_sql=True, save_to_excel=False)
         death_rule_first_55(save_to_sql=True, save_to_excel=True)
         death_rule_second_new(save_to_sql=True, save_to_excel=True)
-        death_escalation(save_to_sql=True, save_to_excel=False)
+        death_escalation(save_to_sql=True, save_to_excel=True)
         print(f'The end of the mortality analysis algorithm. elapsed time {datetime.now() - start_time_ALL}')
         logging.info(f'The end of the mortality analysis algorithm. elapsed time {datetime.now() - start_time_ALL}')
 
