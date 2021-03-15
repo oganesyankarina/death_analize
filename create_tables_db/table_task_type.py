e03f54c1660239bd6f5f1f1819f89eeb28d65d90
oganesyankarina <oganesyankarina@mail.ru>
2021-03-15 13:22:28 +0300

    Изменение оформления в скрипте по генерации списка возможных задач

--- create_tables_db/table_task_type.py
+++ create_tables_db/table_task_type.py
@@ -3,20 +3,29 @@ import pandas as pd
 from sqlalchemy import types
 
 
-def make_table_death_task_type():
+def make_task_type_list():
+    """
+    Функция для генерации списка типов задач, исходя из основных групп МКБ
+    :return: список возможных типов задач
+    """
     task_type = ['Смертность_П1_55+']
     for ind, MKB in enumerate(MKB_GROUP_LIST_MAIN):
         print(ind, MKB)
         task_type.extend([f'Смертность_П2_3monthgrow_{ind}', f'Смертность_П2_sameperiod_{ind}'])
-    print(task_type)
+    return task_type
 
-    df_task_type = pd.DataFrame({'name': task_type})
-    print(df_task_type)
 
-    df_task_type.to_sql('death_task_type', cnx, if_exists='replace', index_label='id', dtype={'name': types.VARCHAR})
+def make_table_death_task_type(task_type_list):
+    """
+    :param task_type_list: спосок возможных типов задач
+    :return: df со списком возможных типов задач
+    """
+    return pd.DataFrame({'name': task_type_list})
 
 
 if __name__ == '__main__':
-    # make_table_death_task_type()
-    pass
-
+    task_type = make_task_type_list()
+    print(f'\n{task_type}\n')
+    df_task_type = make_table_death_task_type(task_type)
+    print(df_task_type)
+    # df_task_type.to_sql('death_task_type', cnx, if_exists='replace', index_label='id', dtype={'name': types.VARCHAR})
